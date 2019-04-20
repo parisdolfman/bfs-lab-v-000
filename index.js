8f5f48dc22c65b51e516b4b452976d72d50fc201
Paris Dolfman <parisdolfman@gmail.com>
2019-04-20 19:44:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function bfs(rootNode, vertices, edges){
 
 }
+
+
+function findAdjacent(node, vertices, edges) {
+  let adjacentNode = edges.filter(edgeLinked => edgeLinked.includes(node))
+  .map(edgeLinked => edgeLinked.filter(vertex => vertex !== node))
+  .map(vertex => vertex[0])
+  return vertices.filter(vertex => adjacentNode.includes(vertex.name)
+  && vertex.distance == null)
+
+}
